891520e4e9cf43d6b32d0bfc19187cf359baee18
Emily Flowers <88520302+emilyflowers@users.noreply.github.com>
2025-11-21 14:39:41 -0500

    Add files via upload

---
+++ src/Lab12Prob01.java
@@ -0,0 +1,29 @@
+/**
+* File: Lab12Prob01.java
+* Class: CSCI 1302
+* Author: Emily Flowers
+* Created on: Nov 21, 2025
+* Last Modified: Nov 21, 2025
+* Description: Create a recursive method
+*/
+
+
+public class Lab12Prob01 {
+	public static void main(String[] args) {
+		System.out.println(recursiveAbstract(3));
+	}
+	
+	// recursive method
+	public static int recursiveAbstract(int n) {
+		if (n == 0) {
+			return 1;
+		} else if (n == 1) {
+			return 3;
+		} else if (n == 2){
+			return 4;
+		} else {
+			return recursiveAbstract(n - 3) * (recursiveAbstract(n - 2) - recursiveAbstract(n - 1));
+		}
+	}
+
+}
